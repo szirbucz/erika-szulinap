ac095a995028d423fc678a34a9cf22eb961636d6
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-06-08 21:04:03 +0200

    Updates

---
+++ precache-manifest.c2e29a24164013445e32abbd4ddc2f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8baf93f3057f55b1a9cf0c97ce680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75668c9a219c38d4dc6f",
+    "url": "/static/css/main.7b9fdde5.chunk.css"
+  },
+  {
+    "revision": "50ca7981738ccb13bff2",
+    "url": "/static/js/2.0195fe87.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0195fe87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75668c9a219c38d4dc6f",
+    "url": "/static/js/main.96f7eb66.chunk.js"
+  },
+  {
+    "revision": "bf0ba9ba021e29d75eb3",
+    "url": "/static/js/runtime-main.27da07b7.js"
+  }
+]);
